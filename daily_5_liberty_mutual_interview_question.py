11b7de0e103e23e6235a8da4db40cec4ecf341f9
khaled <khaledabdel.dev@gmail.com>
2024-11-02 23:13:19 -0500

    updated for correctness, efficency and better syntax

--- daily_5_liberty_mutual_interview_question.py
+++ daily_5_liberty_mutual_interview_question.py
@@ -1,13 +1,22 @@
-# this is was my own iteration, first time while whiteboarding
+# this is the corrected version with my errors fixed
 
-def most_freq_city(cities: List[str] -> str)
+from typing import List # library for lists
+from collections import Counter # library for the counter function
+
+def most_freq_city(cities: List[str]) -> str: # syntax error with the parentheses
 
 	size = len(cities)
 	
 	if size == 0:
 		return 'list empty'
 	elif size == 1:
-		return cities
+		return cities[0] # returns the value as a string as opposed to a single element list
 	elif size > 1:
-		cities.sort()
-		return cities[0]
+		# use collections.Counter() to count iterations of each element in a list
+		city_counter = Counter(cities)
+		# most.common() is a method provided by the Counter class
+		# it returns a tuple, the element and its frequency
+		most_common_city, _  = city_counter.most_common(1)[0] # here we're unpacking the tuple and returning the element individually
+		return most_common_city 
+
+# this function now works recursively btw
