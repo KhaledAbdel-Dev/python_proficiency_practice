2cdc3511ba844e7c722bdf7084f2614b4e853723
khaled <khaledabdel.dev@gmail.com>
2024-11-05 02:00:43 -0500

    7kyu codewars solved... 75% by myself / 25% by claude lol

---
+++ daily_8_population_growth.py
@@ -0,0 +1,11 @@
+def nb_year(p0, percent, aug, p):
+
+	current = p0
+	years_needed = 0
+
+	while current < p:
+		growth = int(current * (percent / 100) + aug)
+		current += growth
+		years_needed += 1
+
+	return years_needed
